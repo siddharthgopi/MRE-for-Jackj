da9be86429e23450056b0210274867dd020e75a6
siddharth gopinathan <leysg1@nottingham.ac.uk>
2014-04-24 02:09:12 +0100

    .

---
+++ MRE for jackson.r
@@ -0,0 +1,34 @@
+library(midasr)
+library(zoo)
+
+
+ yvellaregdata <- read.table("~/Desktop/attempt1/ymonthlyjackson.csv", quote="\"")
+ yvellareg <- ts(yvellaregdata, start=c(2008,7), frequency=12)
+
+ xvellareginit <- read.table("~/Desktop/attempt1/xdailyjackson.csv", quote="\"")
+ xvellaregzoo <- zoo(xvellareg)
+ xvellareg <- as.numeric(xvellaregzoo) #i had to convert to numeric for it to work
+
+#yvellareg is the monthly y variable
+#xvellareg is the daily x variable
+ betareg <- midas_r(yvellareg ~ mls(yvellareg, 1, 1) + mls(xvellareg, 3:25, 30), start=NULL)
+
+
+#Defining data for forecasting
+ xdailyfulldataread <- read.table("~/Desktop/attempt1/xdailyfulldatajackson.csv", quote="\"")
+ xdailyfulldata <- zoo(xdailyfulldataread)
+
+ ymonthlyfulldataread <- read.table("~/Desktop/attempt1/ymonthlyfulldatajackson.csv", quote="\"")
+ ymonthlyfulldata <- ts(ymonthlyfulldataread,start=c(2008,7), frequency=12)
+
+#similar to the example in the userguide
+fulldata <- list(xx=xdailyfulldata,
+                   yy=ymonthfulldata)
+insample <- 1:length(yvellareg)
+outsample <- (1:length(fulldata$yy))[-insample]
+
+avgf<-average_forecast(list(betareg),
+                       data=fulldata,
+                       insample=insample,
+                       outsample=outsample)
+sqrt(avgf$accuracy$individual$MSE.out.of.sample)
